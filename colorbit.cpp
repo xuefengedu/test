efed23f8788445d3cc51074c9fb0535355af2f54
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-26 22:48:24 +0800

    Update colorbit.cpp

--- colorbit.cpp
+++ colorbit.cpp
@@ -2,8 +2,8 @@
 namespace colorbit {
 	        //%
 	        string showColorLeds(ImageLiteral_ leds) {
-                MicroBitImage i(imageBytes(leds));
-                uBit.display.print(MicroBitImage(imageBytes(leds)), 0, 0, 0, 400);
-	        return MicroBitImage(imageBytes(leds)).toString();
+			MicroBitImage i(imageBytes(leds));
+			uBit.display.print(i, 0, 0, 0, 400);
+			return i.toString();
 	        }
 }
