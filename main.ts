932ae996a49ab444052f7c0066124ac8a34fa96e
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-26 22:30:19 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -298,16 +298,14 @@ namespace colorbit {
 	/**
  	 * Draws an image on the ColorBit screen.
  	 * @param leds the pattern of LED to turn on/off
- 	 * @param rgb RGB color of the LED
  	 */
  	//% weight=95 blockGap=8
 	//% imageLiteral=1
 	//% blockId=colorbit_show_color_leds
-	//% block="%colorbit_51bit|show ColorBit leds with color %rgb=colorbit_colors" icon="\uf00a"
+	//% block="%colorbit_51bit|show ColorBit leds with color" icon="\uf00a"
 	//% parts="colorbit" shim=colorbit::showColorLeds
-	showColorLeds(leds: string, rgb: number): void {
+	showColorLeds(leds: string): void {
 	    console.log("color leds: " + leds);
-	    console.log("rgb: " + rgb);
              return;
      	}
 	
