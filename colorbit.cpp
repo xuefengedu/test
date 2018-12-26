0e05f0202ff73183041238b4f6a7bf090385a2ac
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-26 00:15:07 -0800

    Update colorbit.cpp

--- colorbit.cpp
+++ colorbit.cpp
@@ -5,18 +5,7 @@
  */
 //% weight=5 color=#2699BF icon="\uf110"
 namespace colorbit {
-
-         /**
-         * Draws an image on the ColorBit screen.
-         * @param color leds the pattern of LED to turn on/off
-         * @param interval time in milliseconds to pause after drawing
-         */
-        //% weight=95 blockGap=8
-        //% imageLiteral=1 async
-        //% blockId=colorbit_show_color_leds
-        //% block="show ColorBit leds" icon="\uf00a"
-        //% parts="colorbit"
-        void showColorLeds(ImageLiteral_ leds, int interval = 400) {
+        void showColorLeds(ImageLiteral_ leds) {
 	    //MicroBitImage i(imageBytes(leds));
 	    basic.showString(''+leds);
         }
