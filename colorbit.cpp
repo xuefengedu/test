d789cf056a5e3fc1339cd883836612085b7df41e
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-26 21:21:08 +0800

    Update colorbit.cpp

--- colorbit.cpp
+++ colorbit.cpp
@@ -1,7 +1,7 @@
 #include "pxt.h"
 namespace colorbit {
 	        //%
-	        void showColorLeds(ImageLiteral_ leds) {
+	        void showColorLeds(ImageLiteral_ leds, int rgb) {
                 MicroBitImage i(imageBytes(leds));
                 uBit.display.print(MicroBitImage(imageBytes(leds)), 0, 0, 0, 400);
 	        }
