9c69b38a9d1e756378b744940ec69791fa9ee3e3
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-25 23:49:06 -0800

    Update colorbit.d.ts

--- colorbit.d.ts
+++ colorbit.d.ts
@@ -1,18 +1,1 @@
-/**
- * Functions to operate ColorBit
- */
-//% weight=5 color=#2699BF icon="\uf110"
-declare namespace colorbit {
 
-    /**
-	 * Draws an image on the ColorBit screen.
-	 * @param color leds the pattern of LED to turn on/off
-	 * @param interval time in milliseconds to pause after drawing
-	 */
-	//% weight=95 blockGap=8
-	//% imageLiteral=1 async
-	//% blockId=colorbit_show_color_leds
-	//% block="show ColorBit leds" icon="\uf00a"
-    //% parts="colorbit" interval.defl=400 shim=colorbit::showColorLeds
-    function showColorLeds(leds: string, interval?: int32): void;
-}
