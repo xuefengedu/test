90cdfbe4dad27ac7313b307b09e6224dd0b87cd3
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-27 08:04:51 +0800

    Create test.d.ts

---
+++ test.d.ts
@@ -0,0 +1,19 @@
+
+
+/**
+ * 自定义图形块
+ */
+//% weight=100 color=#0fbc11 icon=""
+declare namespace test {
+
+    /**
+	 * Draws an image on the ColorBit screen.
+     * @param leds the pattern of LED to turn on/off
+	 */
+    //% blockId="test_show_color_leds" block="ColorBit leds"
+    //% imageLiteral=1
+    //% weight=95 blockGap=8
+    //% icon="\uf00a"
+    //% parts="test" shim=colorbit::showColorLeds
+    export function showColorBitLeds(leds: string): string;
+}
