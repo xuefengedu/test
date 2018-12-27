ec6f44b840a3550f4abadee1e1cbef8ad8e440ed
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-27 08:43:04 +0800

    Update test.d.ts

--- test.d.ts
+++ test.d.ts
@@ -7,13 +7,13 @@
 declare namespace test {
 
     /**
-	 * Draws an image on the ColorBit screen.
-     * @param leds the pattern of LED to turn on/off
-	 */
+    * Draws an image on the ColorBit screen.
+    * @param leds the pattern of LED to turn on/off
+    */
     //% blockId="test_show_color_leds" block="ColorBit leds"
     //% imageLiteral=1
     //% weight=95 blockGap=8
     //% icon="\uf00a"
-    //% parts="test" shim=colorbit::showColorLeds
-    export function showColorBitLeds(leds: string): string;
+    //% parts="test" shim=test::showColorLeds
+    export function showColorLeds(leds: string): string;
 }
